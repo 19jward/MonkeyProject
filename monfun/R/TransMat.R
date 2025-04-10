939aebf9c1003bab488cc7ae85e556863355c491
Josephine Ward <19jward@gmail.com>
2025-04-10 11:44:40 -0700

    cleaning rmd file + fixing naming continuity

--- monfun/R/TransMat.R
+++ monfun/R/TransMat.R
@@ -1,24 +1,17 @@
-# create transition matrix
-
-behaviorshortlist <-c("Move", "Vigilance", "Feed", "Rest",
-                      "Affiliative", "Aggressive", "SelfGroom",
-                      "Survey", "PAUSE", "Human Directed",
-                      "Forage", "Sleep", "Play")
-
-matrixfunct <- function(dataframe, behaviorshortlist){
-  Matrix <- matrix(NA, nrow = length(behaviorshortlist), ncol = length(behaviorshortlist))
-  rownames(Matrix) <- colnames(Matrix) <- behaviorshortlist
-  for(b1 in behaviorshortlist){
-    denomentator <- sum(dataframe$TimeSpent[dataframe$Behavior == b1]) - 
-      as.numeric(tail(dataframe$Behavior, 1) == b1) ## accounting for if the last behavior in focal is b1 (doesn't count as a transition opportunity), overall total possible transition opportunities
-    b1_ind <- which(dataframe$Behavior == b1)
-    for(b2 in behaviorshortlist){
+matrixfunct <- function(data, behavior_list){
+  Matrix <- matrix(NA, nrow = length(behavior_list), ncol = length(behavior_list))
+  rownames(Matrix) <- colnames(Matrix) <- behavior_list
+  for(b1 in behavior_list){
+    denomentator <- sum(dataframe$TimeSpent[data$Behavior == b1]) - 
+      as.numeric(tail(data$Behavior, 1) == b1) ## accounting for if the last behavior in focal is b1 (doesn't count as a transition opportunity), overall total possible transition opportunities
+    b1_ind <- which(data$Behavior == b1)
+    for(b2 in behavior_list){
       if(b1 == b2){ ## if staying in the same behavior
-        numerator <- denomentator - sum(dataframe$Behavior == b1) + 
-          as.numeric(tail(dataframe$Behavior, 1) == b1) 
-        ## removing the times they switched to a diff behavior & adding back in that situation if the last behavior is b1
+        numerator <- denomentator - sum(data$Behavior == b1) + 
+          as.numeric(tail(data$Behavior, 1) == b1) 
+        ## removing the times they switched to a diff behavior & adding back in that     situation if the last behavior is b1
       } else { ## if moving to a different behavior
-        numerator <- sum(dataframe$Behavior[b1_ind + 1] == b2, na.rm = T)
+        numerator <- sum(data$Behavior[b1_ind + 1] == b2, na.rm = T)
       }
       Matrix[b1,b2] <- numerator/denomentator
     }
